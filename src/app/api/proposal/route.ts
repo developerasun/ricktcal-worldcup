f13f25205f88bd04385d1c07782481b822b35395
developerasun <nellow1102@gmail.com>
2025-07-26 15:24:26 +0900

    feat: add proposal list api

---
+++ src/app/api/proposal/route.ts
@@ -0,0 +1,11 @@
+import { getConnection, proposals, users } from '@/server/database/schema';
+import { UnauthorizedException } from '@/server/error';
+import { lt } from 'drizzle-orm';
+import { NextResponse, NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const { connection } = await getConnection();
+  const data = (await connection.select().from(proposals).where(lt(proposals.id, 25))) ?? [];
+
+  return NextResponse.json(data);
+}
