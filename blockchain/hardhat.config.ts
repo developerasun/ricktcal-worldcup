d87b067478b8d6cd3a17b057432b8185955f714d
developerasun <nellow1102@gmail.com>
2025-07-26 21:02:13 +0900

    feat: init hardhat

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const {
+  ETHERSCAN_SEPOLIA_API_KEY, ALCHEMY_API_ENDPOINT, ROOT_WALLET_PRIVATE_KEY
+} = process.env
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: "0.8.0",
+    networks: {
+    sepolia: {
+      url: ALCHEMY_API_ENDPOINT ? ALCHEMY_API_ENDPOINT : '',
+      accounts: [ROOT_WALLET_PRIVATE_KEY ? ROOT_WALLET_PRIVATE_KEY : '']
+    }
+  },
+    etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_SEPOLIA_API_KEY ? ETHERSCAN_SEPOLIA_API_KEY : ''
+    },
+  },
+};
+
+export default config;
