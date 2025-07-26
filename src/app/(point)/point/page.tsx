d3c94850354360df2d8d5e3e17e4c0678487154e
developerasun <nellow1102@gmail.com>
2025-07-26 16:37:41 +0900

    feat: render reaction on point claim

--- src/app/(point)/point/page.tsx
+++ src/app/(point)/point/page.tsx
@@ -1,19 +1,46 @@
 'use client';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useSpring, animated } from '@react-spring/web';
 import { useDrag } from '@use-gesture/react';
 import { PointClaimActionType } from '@/types/application';
+import Image from 'next/image';
+import { TypographyH1, TypographyP } from '@/components/ui/typography';
+import { Spacer } from '@/components/ui/spacer';
+import { ArrowDownUp, ArrowLeft, ArrowRightLeft, Move } from 'lucide-react';
 
 interface Props {}
 
 export default function PointPage({}: Props) {
+  const [isHeadpatDrag, setIsHeadpatDrag] = useState(false);
+  const [isCheekPullingDrag, setIsCheekPullingDrag] = useState(false);
+
   return (
-    <div>
-      캐릭터의 볼을 당기거나 머리를 쓰다듬고 포인트를 획득하세요.
-      <InteractWithCharacter type="cheekpulling" />
-      asdf
-      <InteractWithCharacter type="headpat" />
-    </div>
+    <>
+      <TypographyH1 text="릭트컬 재화 얻기" />
+      <TypographyP text="캐릭터의 볼을 당기거나 머리를 쓰다듬고 포인트를 획득하세요." align="text-left" />
+
+      <Spacer v={1} />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexFlow: 'row wrap',
+          alignItems: 'flex-start',
+          gap: '2rem',
+        }}
+      >
+        <div>
+          <TypographyP text="버터의 볼을 당긴다" />
+          <InteractWithCharacter type="cheekpulling" onDragStateChange={setIsCheekPullingDrag} />
+          <RenderReaction type="cheekpulling" isDragging={isCheekPullingDrag} />
+        </div>
+        <div>
+          <TypographyP text="버터의 머리를 쓰다듬는다" />
+          <InteractWithCharacter type="headpat" onDragStateChange={setIsHeadpatDrag} />
+          <RenderReaction type="headpat" isDragging={isHeadpatDrag} />
+        </div>
+      </div>
+    </>
   );
 }
 
@@ -21,13 +48,20 @@ export default function PointPage({}: Props) {
  *
  * @doc https://github.com/pmndrs/use-gesture?tab=readme-ov-file#simple-example
  */
-function InteractWithCharacter({ type }: { type: PointClaimActionType }) {
+function InteractWithCharacter({
+  type,
+  onDragStateChange,
+}: {
+  type: PointClaimActionType;
+  onDragStateChange: (isDragging: boolean) => void;
+}) {
   const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
   const isDragDone = useRef<boolean>(null);
 
   // Set the drag hook and define component movement based on gesture data.
   const bind = useDrag(({ down, movement: [mx, my] }) => {
     isDragDone.current = false;
+    onDragStateChange(down);
 
     switch (type) {
       case 'cheekpulling':
@@ -68,9 +102,38 @@ function InteractWithCharacter({ type }: { type: PointClaimActionType }) {
 
   // Bind it to a component.
   return (
-    <animated.div
-      {...bind()}
-      style={{ x, y, touchAction: 'none', width: 100, height: 100, backgroundColor: 'red', borderRadius: '50%' }}
-    />
+    <>
+      <animated.div
+        {...bind()}
+        style={{
+          x,
+          y,
+          touchAction: 'none',
+          width: 50,
+          height: 50,
+          cursor: 'pointer',
+          backgroundColor: 'red',
+          borderRadius: '50%',
+        }}
+      >
+        {type === 'cheekpulling' && <ArrowLeft />}
+        {type === 'headpat' && <Move />}
+      </animated.div>
+    </>
   );
+}
+
+function RenderReaction({ type, isDragging }: { type: PointClaimActionType; isDragging: boolean }) {
+  if (!isDragging) return <Image src={'/포인트/기본.webp'} width={200} height={200} alt="기본" />;
+
+  switch (type) {
+    case 'cheekpulling':
+      return <Image src={'/포인트/볼당기기.webp'} width={200} height={200} alt="볼당기기" />;
+
+    case 'headpat':
+      return <Image src={'/포인트/쓰다듬기.webp'} width={200} height={200} alt="쓰다듬기" />;
+
+    default:
+      <Image src={'/포인트/기본.webp'} width={200} height={200} alt="기본" />;
+  }
 }
---
+++ src/components/ui/spacer.tsx
@@ -0,0 +1,4 @@
+function Spacer({ v }: { v: number }) {
+  return <div style={{ marginTop: `${v}rem`, marginBottom: `${v}rem` }} />;
+}
+export { Spacer };
