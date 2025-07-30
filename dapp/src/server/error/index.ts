5b26e26f5d6a85f270a89187f519bb5bcda74e7a
developerasun <nellow1102@gmail.com>
2025-07-30 23:48:21 +0900

    feat: define base error and usage cases

--- dapp/src/server/error/index.ts
+++ dapp/src/server/error/index.ts
@@ -1,6 +1,53 @@
-export class UnauthorizedException extends Error {
-  #status: number = 401;
-  constructor() {
-    super();
+import { HttpStatus } from '@/constants';
+
+interface IHttpExceptionOptions extends ErrorOptions {
+  path?: string;
+  code?: HttpStatus;
+}
+
+class HttpException extends Error {
+  #status: undefined | HttpStatus = undefined;
+  #path: undefined | string;
+
+  constructor(message?: string, options?: IHttpExceptionOptions) {
+    super(message, options);
+    this.#status = options?.code;
+    this.#path = options?.path;
+  }
+
+  // @dev inherit this context
+  short = () => {
+    return {
+      message: this.message,
+      code: this.#status,
+      path: this.#path,
+    };
+  };
+
+  full() {
+    const _ = this.short();
+    return {
+      ..._,
+      stack: this.stack,
+      cause: this.cause,
+    };
+  }
+}
+
+export class UnauthorizedException extends HttpException {
+  constructor(message?: string, options?: IHttpExceptionOptions) {
+    super(message, options);
+  }
+}
+
+export class BadRequestException extends HttpException {
+  constructor(message?: string, options?: IHttpExceptionOptions) {
+    super(message, options);
+  }
+}
+
+export class NotFoundException extends HttpException {
+  constructor(message?: string, options?: IHttpExceptionOptions) {
+    super(message, options);
   }
 }
