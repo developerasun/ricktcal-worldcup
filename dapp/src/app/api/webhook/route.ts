80605edf52bbd201a758faee9aa29d8be77c5941
developerasun <nellow1102@gmail.com>
2025-07-31 19:07:59 +0900

    feat: add cron webhook for proposal status update

--- dapp/src/app/api/webhook/route.ts
+++ dapp/src/app/api/webhook/route.ts
@@ -1,14 +1,44 @@
+import { ProposalStatus } from '@/constants';
+import { getConnection, proposals } from '@/server/database/schema';
 import { UnauthorizedException } from '@/server/error';
+import { fromUTC, getKoreanTimezone } from '@/server/hook';
+import { and, eq, inArray } from 'drizzle-orm';
 import { NextResponse, NextRequest } from 'next/server';
 
+/**
+ * @swagger
+ * /api/webhook:
+ *   post:
+ *     description: trigger pending proposal to be active
+ *     responses:
+ *       200:
+ *         description: return proposal id on success
+ */
 export async function POST(request: NextRequest) {
-  const { BEARER_TOKEN } = process.env;
-  const token = request.headers.get('authorization')?.split(' ').pop();
-  const isValid = BEARER_TOKEN === token;
+  const { NODE_ENV } = process.env;
 
-  if (!token || !isValid) {
-    throw new UnauthorizedException();
+  if (NODE_ENV === 'production') {
+    const { BEARER_TOKEN } = process.env;
+    const token = request.headers.get('authorization')?.split(' ').pop();
+    const isValid = BEARER_TOKEN === token;
+
+    if (!token || !isValid) {
+      throw new UnauthorizedException();
+    }
   }
 
-  return NextResponse.json({ message: 'ok', isValid, token });
+  const { connection } = await getConnection();
+  const { short: today } = fromUTC();
+
+  const pendings = await connection
+    .select({ id: proposals.id, startAt: proposals.startAt })
+    .from(proposals)
+    .where(and(eq(proposals.status, ProposalStatus.PENDING), eq(proposals.startAt, today)));
+
+  console.log({ today, startAts: pendings.map((p) => p.startAt) });
+
+  const ids = pendings.map((p) => p.id);
+  await connection.update(proposals).set({ status: ProposalStatus.ACTIVE }).where(inArray(proposals.id, ids));
+
+  return NextResponse.json({ proposals: ids });
 }
