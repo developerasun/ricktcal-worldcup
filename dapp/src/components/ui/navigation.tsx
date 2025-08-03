8bc33efefa2710049731c9cd00b6e303dee839ff
developerasun <nellow1102@gmail.com>
2025-08-04 00:24:02 +0900

    refactor: split path for new user

--- dapp/src/components/ui/navigation.tsx
+++ dapp/src/components/ui/navigation.tsx
@@ -2,12 +2,12 @@
 
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useState, ReactNode, useEffect, useActionState } from 'react';
-import { Divide, Moon, Siren, Sun, Wallet } from 'lucide-react';
+import { Moon, Siren, Sun, Wallet } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { IconCopy, IconGithub } from './icon';
+import { IconCopy, IconGithub, IconUserRoundPen } from './icon';
 import {
   Dialog,
   DialogClose,
@@ -23,6 +23,8 @@ import { Alert, AlertTitle, AlertDescription } from './alert';
 import { Spacer } from './spacer';
 import { useCopyText } from '@/lib/client';
 import Link from 'next/link';
+import { useAuth } from '@/app/store';
+import { Separator } from '@/components/ui/separator';
 
 interface Props {
   children: ReactNode;
@@ -32,12 +34,18 @@ export default function Navigation({ children }: Props) {
   const { setTheme } = useTheme();
   const [isDark, setIsDark] = useState(true);
   const [state, formAction] = useActionState(generateWallet, undefined);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isNewUser, setIsNewUser] = useState(false);
 
+  const { auth } = useAuth();
   const router = useRouter();
+
   useEffect(() => {
     isDark ? setTheme('dark') : setTheme('light');
   }, [isDark]);
+
+  useEffect(() => {
+    if (state) setIsNewUser(true);
+  }, [state]);
 
   return (
     <nav className="min-h-screen flex flex-col">
@@ -70,80 +78,94 @@ export default function Navigation({ children }: Props) {
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
         </Button>
-        <Dialog>
-          <DialogTrigger className="border border-gray-600 rounded-sm p-2">
-            <Wallet className="h-[1rem] w-[1rem]" />
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>시작하기</DialogTitle>
-              <DialogDescription>
-                포인트를 엘리프 토큰으로 교환하고, <br /> 나만의 최애 사도에게 투표하세요!
-              </DialogDescription>
-            </DialogHeader>
-            <Form action={formAction}>
-              {state && (
-                <div className="flex flex-col justify-center items-center">
-                  <div className="flex gap-2">
-                    <p>지갑 주소</p>
-                    <IconCopy
-                      className="cursor-pointer"
-                      onClick={async () => {
-                        await useCopyText({ text: state.address.toString() });
-                        alert('지갑 주소가 복사되었습니다.');
-                      }}
-                    />
-                  </div>
-                  <p className="my-2 break-all whitespace-normal w-full">{state.address.toString()}</p>
-                </div>
-              )}
-
-              <Spacer v={1} />
-
-              {state && state.mnemonic && (
-                <div className="flex flex-col justify-center items-center">
-                  <div className="flex gap-2">
-                    <p>지갑 패스키</p>
-                    <IconCopy
-                      className="cursor-pointer"
-                      onClick={async () => {
-                        await useCopyText({ text: state.mnemonic! });
-                        alert('패스키가 복사되었습니다.');
-                      }}
-                    />
-                  </div>
-                  <p className="my-2 break-all whitespace-normal w-full">{state.mnemonic}</p>
-                </div>
-              )}
-              <Spacer v={1.5} />
-              {state && (
-                <Alert variant="destructive">
-                  <Siren />
-                  <AlertTitle>경고</AlertTitle>
-                  <AlertDescription>
-                    생성된 지갑은 테스트넷 전용으로만 사용하고, 지갑 패스키를 타인과 공유하지 마십시오. 패스키 정보는
-                    모달을 닫으면 다시 볼 수 없습니다.
-                  </AlertDescription>
-                </Alert>
-              )}
-              <Spacer v={1.5} />
-              <div className="flex justify-end">
+        {!auth && (
+          <Dialog>
+            <DialogTrigger className="border border-gray-600 rounded-sm p-2">
+              <Wallet className="h-[1rem] w-[1rem]" />
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>시작하기</DialogTitle>
+                <DialogDescription>
+                  포인트를 엘리프 토큰으로 교환하고, <br /> 나만의 최애 사도에게 투표하세요!
+                </DialogDescription>
+              </DialogHeader>
+              <Form action={formAction}>
                 {state && (
-                  <DialogClose asChild className="m-auto">
-                    <Link href={'/login'} className="p-3 m-auto border border-blue-300 rounded-md">
-                      로그인하기
-                    </Link>
-                  </DialogClose>
+                  <div className="flex flex-col justify-center items-center">
+                    <div className="flex gap-2">
+                      <p>지갑 주소</p>
+                      <IconCopy
+                        className="cursor-pointer"
+                        onClick={async () => {
+                          await useCopyText({ text: state.address.toString() });
+                          alert('지갑 주소가 복사되었습니다.');
+                        }}
+                      />
+                    </div>
+                    <p className="my-2 break-all whitespace-normal w-full">{state.address.toString()}</p>
+                  </div>
                 )}
-                {!state && (
-                  <Button type="submit" className="m-auto">
-                    패스키 생성하기
-                  </Button>
+
+                <Spacer v={1} />
+
+                {state && state.mnemonic && (
+                  <div className="flex flex-col justify-center items-center">
+                    <div className="flex gap-2">
+                      <p>지갑 패스키</p>
+                      <IconCopy
+                        className="cursor-pointer"
+                        onClick={async () => {
+                          await useCopyText({ text: state.mnemonic! });
+                          alert('패스키가 복사되었습니다.');
+                        }}
+                      />
+                    </div>
+                    <p className="my-2 break-all whitespace-normal w-full">{state.mnemonic}</p>
+                  </div>
                 )}
-              </div>
-            </Form>
-          </DialogContent>
-        </Dialog>
+                <Spacer v={1.5} />
+                {state && (
+                  <Alert variant="destructive">
+                    <Siren />
+                    <AlertTitle>경고</AlertTitle>
+                    <AlertDescription>
+                      생성된 지갑은 테스트넷 전용으로만 사용하고, 지갑 패스키를 타인과 공유하지 마십시오. 패스키 정보는
+                      모달을 닫으면 다시 볼 수 없습니다.
+                    </AlertDescription>
+                  </Alert>
+                )}
+                <Spacer v={1.5} />
+                <div className="flex justify-end">
+                  {state && (
+                    <DialogClose asChild className="m-auto">
+                      <Link href={'/login'} className="p-3 m-auto border border-blue-300 rounded-md">
+                        로그인하기
+                      </Link>
+                    </DialogClose>
+                  )}
+                  {!state && (
+                    <Button type="submit" className="m-auto">
+                      패스키 생성하기
+                    </Button>
+                  )}
+                </div>
+              </Form>
+              {!isNewUser && (
+                <DialogClose asChild className="m-auto">
+                  <Link href={'/login'} className="p-3 m-auto border border-blue-300 rounded-md">
+                    패스키가 이미 있어요
+                  </Link>
+                </DialogClose>
+              )}
+            </DialogContent>
+          </Dialog>
+        )}
+        {auth && (
+          <Button variant="outline" size="icon" onClick={() => router.push(`/profile/${auth.wallet}`)}>
+            <IconUserRoundPen className="h-[1.2rem] w-[1.2rem]" />
+          </Button>
+        )}
       </div>
 
       {/* prettier-ignore */}
