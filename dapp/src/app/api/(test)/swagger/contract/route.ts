d669155472001b511bc1b952d22299189dda3474
developerasun <nellow1102@gmail.com>
2025-08-06 19:21:19 +0900

    refactor: guard prod swagger routes for cost-sensitive endpoints

--- dapp/src/app/api/(test)/swagger/contract/route.ts
+++ dapp/src/app/api/(test)/swagger/contract/route.ts
@@ -1,6 +1,7 @@
-import { proposals, votes, users, getConnection } from '@/server/database/schema';
+import { HttpStatus } from '@/constants';
+import { ForbiddenException } from '@/server/error';
 import { Elif } from '@/server/hook';
-import { eq } from 'drizzle-orm';
+import { NextResponse } from 'next/server';
 
 /**
  * @swagger
@@ -12,6 +13,20 @@ import { eq } from 'drizzle-orm';
  *         description: return Elif's token name and symbol
  */
 export async function GET(request: Request, context: any) {
+  const { NODE_ENV } = process.env;
+
+  if (NODE_ENV === 'production') {
+    const { BEARER_TOKEN } = process.env;
+    const token = request.headers.get('authorization')?.split(' ').pop();
+    const isValid = BEARER_TOKEN === token;
+
+    if (!token || !isValid) {
+      const e = new ForbiddenException('유효하지 않은 api 키입니다.', { code: HttpStatus.FORBIDDEN });
+      const response = e.short();
+      return NextResponse.json(response);
+    }
+  }
+
   const { elif } = new Elif().instance();
   const name = await elif.name();
   const symbol = await elif.symbol();
--- dapp/src/app/api/(test)/swagger/query/route.ts
+++ dapp/src/app/api/(test)/swagger/query/route.ts
@@ -1,5 +1,7 @@
-import { proposals, votes, users, getConnection } from '@/server/database/schema';
-import { eq } from 'drizzle-orm';
+import { HttpStatus } from '@/constants';
+import { getConnection } from '@/server/database/schema';
+import { ForbiddenException } from '@/server/error';
+import { NextResponse } from 'next/server';
 
 /**
  * @swagger
@@ -11,6 +13,20 @@ import { eq } from 'drizzle-orm';
  *         description: entities quried by drizzle
  */
 export async function GET(request: Request, context: any) {
+  const { NODE_ENV } = process.env;
+
+  if (NODE_ENV === 'production') {
+    const { BEARER_TOKEN } = process.env;
+    const token = request.headers.get('authorization')?.split(' ').pop();
+    const isValid = BEARER_TOKEN === token;
+
+    if (!token || !isValid) {
+      const e = new ForbiddenException('유효하지 않은 api 키입니다.', { code: HttpStatus.FORBIDDEN });
+      const response = e.short();
+      return NextResponse.json(response);
+    }
+  }
+
   const { connection } = await getConnection();
   const forcedDecimals = await connection.run(`select ROUND(3.4141422 - 1.155, 2)`);
 
