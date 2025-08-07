e7e5c245d8ec6a7af0cdd0dc6a20d8112a4ee3ec
developerasun <nellow1102@gmail.com>
2025-08-07 19:34:55 +0900

    build: update abi to viem-compatible format

--- dapp/src/constants/index.ts
+++ dapp/src/constants/index.ts
@@ -798,38 +798,594 @@ const COOKIE_NAME = {
 // ================================================================== //
 const ABI_HELPER = {
   elif: [
-    'constructor()',
-    'error ERC20InsufficientAllowance(address spender, uint256 allowance, uint256 needed)',
-    'error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed)',
-    'error ERC20InvalidApprover(address approver)',
-    'error ERC20InvalidReceiver(address receiver)',
-    'error ERC20InvalidSender(address sender)',
-    'error ERC20InvalidSpender(address spender)',
-    'error OwnableInvalidOwner(address owner)',
-    'error OwnableUnauthorizedAccount(address account)',
-    'event Approval(address indexed owner, address indexed spender, uint256 value)',
-    'event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)',
-    'event Transfer(address indexed from, address indexed to, uint256 value)',
-    'function allowance(address owner, address spender) view returns (uint256)',
-    'function approve(address spender, uint256 value) returns (bool)',
-    'function balanceOf(address account) view returns (uint256)',
-    'function burn(uint256 value)',
-    'function burn(address account, uint256 amount)',
-    'function burnFrom(address account, uint256 value)',
-    'function castVote(uint256 p, address v, (bytes32 digest, bytes signature, bool hasVoted) vc, uint256 amount)',
-    'function decimals() view returns (uint8)',
-    'function getRecoveredSigner(bytes32 digest, bytes signature) pure returns (address)',
-    'function getVoteCastByProposal(uint256 p, address v) view returns ((bytes32 digest, bytes signature, bool hasVoted) vc)',
-    'function hasVoted(uint256 p, address v) view returns (bool)',
-    'function mint(address to, uint256 amount)',
-    'function name() view returns (string)',
-    'function owner() view returns (address)',
-    'function renounceOwnership()',
-    'function symbol() view returns (string)',
-    'function totalSupply() view returns (uint256)',
-    'function transfer(address to, uint256 value) returns (bool)',
-    'function transferFrom(address from, address to, uint256 value) returns (bool)',
-    'function transferOwnership(address newOwner)',
+    {
+      inputs: [],
+      stateMutability: 'nonpayable',
+      type: 'constructor',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'spender',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'allowance',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: 'needed',
+          type: 'uint256',
+        },
+      ],
+      name: 'ERC20InsufficientAllowance',
+      type: 'error',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'sender',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'balance',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: 'needed',
+          type: 'uint256',
+        },
+      ],
+      name: 'ERC20InsufficientBalance',
+      type: 'error',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'approver',
+          type: 'address',
+        },
+      ],
+      name: 'ERC20InvalidApprover',
+      type: 'error',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'receiver',
+          type: 'address',
+        },
+      ],
+      name: 'ERC20InvalidReceiver',
+      type: 'error',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'sender',
+          type: 'address',
+        },
+      ],
+      name: 'ERC20InvalidSender',
+      type: 'error',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'spender',
+          type: 'address',
+        },
+      ],
+      name: 'ERC20InvalidSpender',
+      type: 'error',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+      ],
+      name: 'OwnableInvalidOwner',
+      type: 'error',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'account',
+          type: 'address',
+        },
+      ],
+      name: 'OwnableUnauthorizedAccount',
+      type: 'error',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'spender',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'Approval',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'previousOwner',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'newOwner',
+          type: 'address',
+        },
+      ],
+      name: 'OwnershipTransferred',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'from',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'to',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'Transfer',
+      type: 'event',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+        {
+          internalType: 'address',
+          name: 'spender',
+          type: 'address',
+        },
+      ],
+      name: 'allowance',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'spender',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'approve',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'account',
+          type: 'address',
+        },
+      ],
+      name: 'balanceOf',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'burn',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'account',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'amount',
+          type: 'uint256',
+        },
+      ],
+      name: 'burn',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'account',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'burnFrom',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'Proposal',
+          name: 'p',
+          type: 'uint256',
+        },
+        {
+          internalType: 'Voter',
+          name: 'v',
+          type: 'address',
+        },
+        {
+          components: [
+            {
+              internalType: 'bytes32',
+              name: 'digest',
+              type: 'bytes32',
+            },
+            {
+              internalType: 'bytes',
+              name: 'signature',
+              type: 'bytes',
+            },
+            {
+              internalType: 'bool',
+              name: 'hasVoted',
+              type: 'bool',
+            },
+          ],
+          internalType: 'struct Elif.VoteCast',
+          name: 'vc',
+          type: 'tuple',
+        },
+        {
+          internalType: 'uint256',
+          name: 'amount',
+          type: 'uint256',
+        },
+      ],
+      name: 'castVote',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'decimals',
+      outputs: [
+        {
+          internalType: 'uint8',
+          name: '',
+          type: 'uint8',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'bytes32',
+          name: 'digest',
+          type: 'bytes32',
+        },
+        {
+          internalType: 'bytes',
+          name: 'signature',
+          type: 'bytes',
+        },
+      ],
+      name: 'getRecoveredSigner',
+      outputs: [
+        {
+          internalType: 'address',
+          name: '',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'pure',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'Proposal',
+          name: 'p',
+          type: 'uint256',
+        },
+        {
+          internalType: 'Voter',
+          name: 'v',
+          type: 'address',
+        },
+      ],
+      name: 'getVoteCastByProposal',
+      outputs: [
+        {
+          components: [
+            {
+              internalType: 'bytes32',
+              name: 'digest',
+              type: 'bytes32',
+            },
+            {
+              internalType: 'bytes',
+              name: 'signature',
+              type: 'bytes',
+            },
+            {
+              internalType: 'bool',
+              name: 'hasVoted',
+              type: 'bool',
+            },
+          ],
+          internalType: 'struct Elif.VoteCast',
+          name: 'vc',
+          type: 'tuple',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'Proposal',
+          name: 'p',
+          type: 'uint256',
+        },
+        {
+          internalType: 'Voter',
+          name: 'v',
+          type: 'address',
+        },
+      ],
+      name: 'hasVoted',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'to',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'amount',
+          type: 'uint256',
+        },
+      ],
+      name: 'mint',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'name',
+      outputs: [
+        {
+          internalType: 'string',
+          name: '',
+          type: 'string',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'owner',
+      outputs: [
+        {
+          internalType: 'address',
+          name: '',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'renounceOwnership',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'symbol',
+      outputs: [
+        {
+          internalType: 'string',
+          name: '',
+          type: 'string',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'to',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'transfer',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'from',
+          type: 'address',
+        },
+        {
+          internalType: 'address',
+          name: 'to',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'transferFrom',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'newOwner',
+          type: 'address',
+        },
+      ],
+      name: 'transferOwnership',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
   ],
 } as const;
 
