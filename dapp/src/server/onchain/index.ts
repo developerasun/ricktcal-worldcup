c59a7a48489134168400829191d8ecf4f6ac8982
developerasun <nellow1102@gmail.com>
2025-08-07 19:34:33 +0900

    refactor: migrate contract write call from ethers to viem for cf worker runtime

--- dapp/src/server/onchain/index.ts
+++ dapp/src/server/onchain/index.ts
@@ -5,7 +5,7 @@ import { JsonRpcProvider, Contract, Wallet, BigNumberish, BytesLike, parseEther,
 import { logger } from '../logger';
 import { NotFoundException } from '../error';
 import { privateKeyToAccount } from 'viem/accounts';
-import { http, createWalletClient, getContract, Abi } from 'viem';
+import { http, createWalletClient, createPublicClient, getContract, Abi } from 'viem';
 import { sepolia } from 'viem/chains';
 
 export class Elif {
@@ -23,19 +23,24 @@ export class Elif {
 
     const account = privateKeyToAccount(ROOT_WALLET_PRIVATE_KEY as `0x${string}`);
 
-    const client = createWalletClient({
+    const walletClient = createWalletClient({
       account,
+      chain: sepolia,
+      transport: http(ALCHEMY_API_ENDPOINT),
+    });
+
+    const publicClient = createPublicClient({
       chain: sepolia,
       transport: http(ALCHEMY_API_ENDPOINT),
     });
 
     const elif = getContract({
       address: ELIF_ADDRESS as `0x${string}`,
-      abi,
-      client,
+      abi: ABI_HELPER.elif,
+      client: walletClient,
     });
 
-    return { elif };
+    return { elif, walletClient, publicClient };
   }
 
   getInstance() {
@@ -81,14 +86,14 @@ export async function tryWithBackOff(callback: () => Promise<void>, baseDelay = 
 //   };
 //   amount: BigNumberish;
 // }) {
-//   const { elif, provider } = new Elif().getInstance();
+//   const { elif, client } = new Elif().getInstance();
 //   let hash: null | string = null;
 //   let nonce: null | number = null;
 //   let hasTracked = false;
 
 //   const target = async () => {
 //     const safeAmount = parseEther(amount.toString());
-//     const response = await elif.castVote(proposalId, voter, voteCast, safeAmount);
+//     const response = await elif.write.castVote([proposalId, voter, voteCast, safeAmount]);
 //     hash = response.hash;
 //     nonce = response.nonce;
 //   };
@@ -114,626 +119,34 @@ export async function tryWithBackOff(callback: () => Promise<void>, baseDelay = 
 //   return { isSuccess, hasTracked, hash, nonce };
 // }
 
-// export async function txMint({ to, amount }: { to: string; amount: BigNumberish }) {
-//   const { elif } = new Elif().getInstance();
-//   let hash: string = '';
-//   let nonce: number = -1;
-//   let hasTracked = false;
+export async function txMint({ to, amount }: { to: `0x${string}`; amount: BigNumberish }) {
+  const { elif, publicClient } = new Elif().getInstance();
+  let hash: `0x${string}` = '0x';
+  let nonce: number = -1;
+  let hasTracked = false;
 
-//   const mint = async () => {
-//     const safeAmount = parseEther(amount.toString());
-//     const response = await elif.write.mint([to, safeAmount]);
-//     hash = response.hash;
-//     nonce = response.nonce;
-//   };
+  const mint = async () => {
+    const safeAmount = parseEther(amount.toString());
+    hash = await elif.write.mint([to, safeAmount]);
+  };
 
-//   const { isSuccess } = await tryWithBackOff(mint);
+  const { isSuccess } = await tryWithBackOff(mint);
 
-//   if (isSuccess && hash.length !== 0 && nonce !== -1) {
-//     const track = async () => {
-//       const hasReceipt = await provider.getTransactionReceipt(hash);
-//       if (!hasReceipt) {
-//         logger.warn(`txMint:track: target hash not found, throwing and retrying ...`);
-//         throw new NotFoundException('존재하지 않거나 아직 블록에 포함되지 않은 트랜잭션 해시입니다.', {
-//           code: HttpStatus.NOT_FOUND,
-//         });
-//       }
+  if (isSuccess && hash !== '0x') {
+    const track = async () => {
+      const hasReceipt = await publicClient.getTransactionReceipt({ hash });
+      if (!hasReceipt) {
+        logger.warn(`txMint:track: target hash not found, throwing and retrying ...`);
+        throw new NotFoundException('존재하지 않거나 아직 블록에 포함되지 않은 트랜잭션 해시입니다.', {
+          code: HttpStatus.NOT_FOUND,
+        });
+      }
 
-//       logger.info(`txMint:track: hash(${hasReceipt.hash}) found with nonce(${nonce})`);
-//     };
-//     const tracking = await tryWithBackOff(track);
-//     hasTracked = tracking.isSuccess;
-//   }
-//   return { isSuccess, hasTracked, hash, nonce };
-// }
-
-const abi = [
-  {
-    inputs: [],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'spender',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'allowance',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'needed',
-        type: 'uint256',
-      },
-    ],
-    name: 'ERC20InsufficientAllowance',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'sender',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'balance',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'needed',
-        type: 'uint256',
-      },
-    ],
-    name: 'ERC20InsufficientBalance',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'approver',
-        type: 'address',
-      },
-    ],
-    name: 'ERC20InvalidApprover',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'receiver',
-        type: 'address',
-      },
-    ],
-    name: 'ERC20InvalidReceiver',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'sender',
-        type: 'address',
-      },
-    ],
-    name: 'ERC20InvalidSender',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'spender',
-        type: 'address',
-      },
-    ],
-    name: 'ERC20InvalidSpender',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'owner',
-        type: 'address',
-      },
-    ],
-    name: 'OwnableInvalidOwner',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-    ],
-    name: 'OwnableUnauthorizedAccount',
-    type: 'error',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'owner',
-        type: 'address',
-      },
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'spender',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'value',
-        type: 'uint256',
-      },
-    ],
-    name: 'Approval',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'previousOwner',
-        type: 'address',
-      },
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address',
-      },
-    ],
-    name: 'OwnershipTransferred',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'from',
-        type: 'address',
-      },
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'value',
-        type: 'uint256',
-      },
-    ],
-    name: 'Transfer',
-    type: 'event',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'owner',
-        type: 'address',
-      },
-      {
-        internalType: 'address',
-        name: 'spender',
-        type: 'address',
-      },
-    ],
-    name: 'allowance',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'spender',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'value',
-        type: 'uint256',
-      },
-    ],
-    name: 'approve',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-    ],
-    name: 'balanceOf',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'value',
-        type: 'uint256',
-      },
-    ],
-    name: 'burn',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256',
-      },
-    ],
-    name: 'burn',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'value',
-        type: 'uint256',
-      },
-    ],
-    name: 'burnFrom',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'Proposal',
-        name: 'p',
-        type: 'uint256',
-      },
-      {
-        internalType: 'Voter',
-        name: 'v',
-        type: 'address',
-      },
-      {
-        components: [
-          {
-            internalType: 'bytes32',
-            name: 'digest',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'bytes',
-            name: 'signature',
-            type: 'bytes',
-          },
-          {
-            internalType: 'bool',
-            name: 'hasVoted',
-            type: 'bool',
-          },
-        ],
-        internalType: 'struct Elif.VoteCast',
-        name: 'vc',
-        type: 'tuple',
-      },
-      {
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256',
-      },
-    ],
-    name: 'castVote',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'decimals',
-    outputs: [
-      {
-        internalType: 'uint8',
-        name: '',
-        type: 'uint8',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'bytes32',
-        name: 'digest',
-        type: 'bytes32',
-      },
-      {
-        internalType: 'bytes',
-        name: 'signature',
-        type: 'bytes',
-      },
-    ],
-    name: 'getRecoveredSigner',
-    outputs: [
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
-    ],
-    stateMutability: 'pure',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'Proposal',
-        name: 'p',
-        type: 'uint256',
-      },
-      {
-        internalType: 'Voter',
-        name: 'v',
-        type: 'address',
-      },
-    ],
-    name: 'getVoteCastByProposal',
-    outputs: [
-      {
-        components: [
-          {
-            internalType: 'bytes32',
-            name: 'digest',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'bytes',
-            name: 'signature',
-            type: 'bytes',
-          },
-          {
-            internalType: 'bool',
-            name: 'hasVoted',
-            type: 'bool',
-          },
-        ],
-        internalType: 'struct Elif.VoteCast',
-        name: 'vc',
-        type: 'tuple',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'Proposal',
-        name: 'p',
-        type: 'uint256',
-      },
-      {
-        internalType: 'Voter',
-        name: 'v',
-        type: 'address',
-      },
-    ],
-    name: 'hasVoted',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256',
-      },
-    ],
-    name: 'mint',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'name',
-    outputs: [
-      {
-        internalType: 'string',
-        name: '',
-        type: 'string',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'owner',
-    outputs: [
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'renounceOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'symbol',
-    outputs: [
-      {
-        internalType: 'string',
-        name: '',
-        type: 'string',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'totalSupply',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'value',
-        type: 'uint256',
-      },
-    ],
-    name: 'transfer',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'from',
-        type: 'address',
-      },
-      {
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'value',
-        type: 'uint256',
-      },
-    ],
-    name: 'transferFrom',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address',
-      },
-    ],
-    name: 'transferOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-] as const;
+      nonce = (await publicClient.getTransaction({ hash })).nonce;
+      logger.info(`txMint:track: hash(${hasReceipt.transactionHash}) found with nonce(${nonce})`);
+    };
+    const tracking = await tryWithBackOff(track);
+    hasTracked = tracking.isSuccess;
+  }
+  return { isSuccess, hasTracked, hash, nonce };
+}
